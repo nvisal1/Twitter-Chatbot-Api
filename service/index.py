13dbcbff7f66adbf243e45f5b9d3a2bb1c15a2c1
Nick Winner <design@winnerdigital.net>
2019-05-16 12:55:17 -0400

    :sparkles: Implement functionality for service to parse Twitter API creds

--- service/index.py
+++ service/index.py
@@ -19,4 +19,4 @@ def convertToJson(tweet):
     return tweet._json
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- service/twitterDriver.py
+++ service/twitterDriver.py
@@ -2,12 +2,14 @@ import tweepy 		#https://github.com/tweepy/tweepy
 import sys
 import csv
 import re
+import os
+import json
 
-#Twitter API credentials
-# consumer_key = ""
-# consumer_secret = ""
-# access_key = ""
-# access_secret = ""
+# Twitter API credentials
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
 
 def get_all_tweets(query):	
 	#--- authorize twitter, initialize tweepy
@@ -19,4 +21,20 @@ def get_all_tweets(query):
 	alltweets = []
 	alltweets.extend(tweepy.Cursor(api.search, q=query, rpp=10, result_type="recent", include_entities=True, lang="en").items(10))
 
-	return alltweets;
+	return alltweets
+
+def creds():
+  global consumer_key, consumer_secret, access_key, access_secret
+
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+
+  with open(dir_path + '/creds.json', 'r') as f:
+    credentials = json.load(f)
+
+    consumer_key = credentials['consumer_key']
+    consumer_secret = credentials['consumer_secret']
+    access_key = credentials['access_key']
+    access_secret = credentials['access_secret']
+
+creds()
+
